ed9fc1319d3bc4c1af7368150e72c0159e8fa2ee
AnthCap96 <anthonycapoccia22@gmail.com>
2024-02-06 16:28:40 +0100

    add script/component name/data option/methods + end script

---
+++ vue-todolist/src/App.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+
+//Component name:
+  name: "App",
+
+//Data option:
+  data() {
+    return {
+      todos: [],
+    };
+  },
+  //Methods option:
+  methods: {
+    addTodo() {
+      if (this.newTodo) {
+        this.todos.push({ text: this.newTodo, done: false });
+        this.newTodo = "";
+      }
+    },
+  },
+}; //End script
+</script>
+
+<template>
+  <div>
+    
+  </div>
+  
+
+</template>
+
+<style>
+</style>
