d7ab32345796b4e9984fe434db8635e290f07b00
AnthCap96 <anthonycapoccia22@gmail.com>
2024-02-07 16:22:21 +0100

    end exercise
    ended exercise

--- vue-todolist/src/App.vue
+++ vue-todolist/src/App.vue
@@ -20,15 +20,24 @@ export default {
     },
   },
 }; //End script
+
 </script>
 
 <template>
-  <div>
-    
+  <div class="container my-20 max-w-lg">
+    <!-- Add todo list -->
+    <div class="flex">
+      <input class="rounded" type="text" v-model="newTodo" placeholder="To Do List" @keyup.enter="addTodo" />
+      <button class="ml-4 rounded bg-black px-6 text-white" @click="addTodo">Add</button>
+    </div>
+
+    <!-- List of todos -->
+    <ul class="mt-8">
+      <li class="mt-4 flex items-center rounded bg-slate-200 p-4" v-for="(todo, index) in todos" :key="index">
+        <input class="rounded border-0 " type="checkbox" v-model="todo.done"/>
+        <div class="ml-3 mr-8">{{ todo.text }}</div>
+        <button class="ml-auto text-red-400 line-through decoration-1 line-through-2" @click="todos.splice(index, 1)">Cancel</button>
+      </li>
+    </ul>
   </div>
-  
-
 </template>
-
-<style>
-</style>
